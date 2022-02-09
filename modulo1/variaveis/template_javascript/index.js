7bf6ec356644b1d24600f6988350d701e564747b
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-02-08 21:40:10 -0300

    acrescentando informações na primeira parte do exercício

--- modulo1/variaveis/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/index.js
@@ -1,6 +1,36 @@
-// 1. 10, 5
-// 2. 10, 10, 10
-// 3. diaQtdHora e diaValor
+/* 1. resposta = 10, 5
+let a = 10 
+let b = 10
+as variáveis a e b receberam a atribuição 10
+
+console.log(b)
+mostra o valor de b que é 10
+
+b = 5
+b recebe outro balor, 5
+
+console.log(a, b)
+os valores de a, b = 10, 5
+
+*/
+/* 2. resposta = 10, 10, 10
+let a = 10
+let b = 20
+c = a -> c recebe 10, valor de a
+b = c -> b recebe 10, valor de c
+a = b -> a recebe 10, valor de b
+
+console.log(a, b, c) -> resposta 10, 10, 10
+
+
+*/
+/* 3. resposta = diaQtdHora e diaValor
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+*/
 
 // Exercício 1
 let nomeUsuario
