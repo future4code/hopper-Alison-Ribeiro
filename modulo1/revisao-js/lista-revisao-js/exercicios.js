feb3009844f5dc8ae738aebf4565fcb171186569
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-03-15 19:45:46 -0300

    Exercício 5

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -35,7 +35,14 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let numeroPar = []
+    for(let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+          numeroPar.push((array[i]*array[i]))
+      }    
+    }
+   return numeroPar
+   
 }
 
 // EXERCÍCIO 06
