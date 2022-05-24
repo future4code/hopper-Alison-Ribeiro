d674c0dfd16519eec7c5a0777d60cec722701917
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-05-24 00:00:23 -0300

    iniciado projeto, install bibliotecas

---
+++ modulo3/projeto-labefy/labefy-project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+
+function App() {
+  return (
+    <div>
+      Olá mundo!!!
+    </div>
+  );
+}
+
+export default App;
