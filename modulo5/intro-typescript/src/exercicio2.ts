8f1edd18a538c64771deb47ce428bfab00a5db35
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-08-09 20:54:19 -0300

    Realizado Exercício Intro TypeScript

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,4 @@
+export const imprimeTresCoresFavoritas = (cor1: string, cor2: string, cor3: string) => {
+
+    return([cor1, cor2, cor3])
+ }
---
+++ modulo5/intro-typescript/src/index.ts
@@ -0,0 +1,21 @@
+import { imprimeTresCoresFavoritas } from './exercicio2';
+import { comparaDoisNumeros } from './exercicio4';
+import { checaTriangulo } from './exercicio1';
+import { checaAnoBissexto } from './exercicio3';
+import { checaRenovacaoRG } from './exercicio5';
+
+//Exercicio 1
+console.log(checaTriangulo(1, 1, 1))
+
+//Exercicio 2
+console.log(imprimeTresCoresFavoritas("azul", "vermelho", "branco"))
+
+//Exercicio 3
+console.log(checaAnoBissexto(2020))
+console.log(checaAnoBissexto(1982))
+
+//Exercicio 4
+console.log(comparaDoisNumeros(5,2))
+
+//Exercicio 5
+console.log(checaRenovacaoRG(2022, 1982, 2019))
