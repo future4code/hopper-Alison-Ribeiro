d0da1adde912a5c5da5ff7ab778d3fdbbeafb866
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-09-14 17:41:08 -0300

    Filtros e ordenacao

--- modulo5/filtros-ordenacao-paginacao/src/endpoints/getAllUsers.ts
+++ modulo5/filtros-ordenacao-paginacao/src/endpoints/getAllUsers.ts
@@ -1,40 +1,20 @@
 import { Request, Response } from "express"
-import {selectAllUsers, selectNameUser} from '../query/queryGetAllUsers'
-import connection from "../database/connection"
-
+import selectAllUsers from "./queryGetAllUsers"
 
 export const getAllUsers = async(req: Request,res: Response): Promise<void> =>{
-    try {
-       const users = await selectAllUsers()
- 
-       if(!users.length){
-          res.statusCode = 404
-          throw new Error("No recipes found")
-       }
- 
-       res.status(200).send(users)
-       
-    } catch (error) {
-       console.log(error)
-       res.send(error.message || error.sqlMessage)
-    }
- }
+   let statusCode = 400
+   try {
+      const users = await selectAllUsers()
 
- // Exercicio 1
- export const getUser = async(req: Request,res: Response): Promise<void> =>{
-    let user = req.query.name
-    try {
-       const users = await await connection.("aula48_exercicio").where("name", "like", `%${user}%`)
- 
-       if(!users.length){
-          res.statusCode = 404
-          throw new Error("No recipes found")
-       }
- 
-       res.status(200).send(users)
-       
-    } catch (error) {
-       console.log(error)
-       res.send(error.message || error.sqlMessage)
-    }
- }
+      if(!users.length){
+         res.statusCode = 404
+         throw new Error("No recipes found")
+      }
+
+      res.status(200).send(users)
+
+   } catch (error: any) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+}
--- modulo5/filtros-ordenacao-paginacao/src/index.ts
+++ modulo5/filtros-ordenacao-paginacao/src/index.ts
@@ -2,5 +2,8 @@ import {app} from './app'
 // import {} from './endpoints/get'
 // import {} from './endpoints/post'
 import {getAllUsers} from './endpoints/getAllUsers'
+import {getUserName} from './endpoints/getUserName/getUserName'
 
-app.get("/allusers", getAllUsers)
+app.get("/allusers", getAllUsers);
+
+app.get("user/:name", getUserName)
